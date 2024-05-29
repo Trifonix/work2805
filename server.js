2d3d7a94d885e2808468499d04781972e07e231a
Trifonix <trifonix@rocketmail.com>
2024-05-29 16:52:00 +0300

    fix submit func

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', (socket) => {
-    console.log('Пользователь подключен');
+io.on("connection", (socket) => {
+  console.log("Пользователь подключен");
 
-    socket.on('char message', (msg) => {
-        io.emit('chat message', msg);
-    });
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
 
-    socket.on('disconnect', () => {
-        console.log('Пользователь отключен');
-    });
+  socket.on("disconnect", () => {
+    console.log("Пользователь отключен");
+  });
 });
 
 server.listen(3000, () => {
-    console.log('Сервер запущен через порт 3000');
+  console.log("Сервер запущен через порт 3000");
 });

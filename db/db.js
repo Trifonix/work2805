f12fc7711eaa00dfa4dcb1f1feb336810aa951b2
Trifonix <trifonix@rocketmail.com>
2024-06-02 16:10:19 +0300

    fix connection to db

--- db/db.js
+++ db/db.js
@@ -1,18 +1,18 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'db_for_chat'
+  host: "MySQL-8.2",
+  user: "root",
+  password: "",
+  database: "db_for_chat",
 });
 
 connection.connect((err) => {
   if (err) {
-    console.error('Ошибка подключения к базе данных:', err);
+    console.error("Ошибка подключения к базе данных:", err);
     return;
   }
-  console.log('Подключение к базе данных MySQL успешно установлено');
+  console.log("Подключение к базе данных MySQL успешно установлено");
 });
 
 module.exports = connection;
